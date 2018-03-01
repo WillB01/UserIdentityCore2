d4a947927d8e6d1725a110666db1bf484bca00b1
WillB01 <wb.william@outlook.com>
2018-03-01 10:19:48 +0100

    can now create users

--- UserIdentityCore2/Controllers/AdminController.cs
+++ UserIdentityCore2/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UserIdentityCore2.Models;
+using UserIdentityCore2.ViewModels;
 
 namespace UserIdentityCore2.Controllers
 {
@@ -18,6 +19,39 @@ namespace UserIdentityCore2.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser()
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ UserIdentityCore2/ViewModels/CreateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserIdentityCore2.ViewModels
+{
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
